f78913b5b0eb27e7c89f5a006771f443c8592a37
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-01-06 12:50:10 +0300

    Add primitive options parser for cli

--- src/cli/Main.hs
+++ src/cli/Main.hs
@@ -1,6 +1,42 @@
 module Main where
 
-import Universum
+import           Data.Version               (showVersion)
+import           Options.Applicative.Simple (Parser, help, long, metavar, simpleOptions,
+                                             strOption, switch, value)
+import           Universum
+
+import           Paths_orgstat              (version)
+
+data Args = Args
+    { configPath :: !FilePath
+      -- ^ Path to configuration file.
+    , debug      :: Bool
+      -- ^ Enable debug logging
+    } deriving Show
+
+argsParser :: Parser Args
+argsParser =
+    Args <$>
+    strOption
+        (long "conf-path" <> metavar "FILEPATH" <> value "~/.orgstat.conf" <>
+        help "Path to the configuration file") <*>
+    switch
+        (long "debug" <>
+         help
+             "Enable debug logging")
+
+getNodeOptions :: IO Args
+getNodeOptions = do
+    (res, ()) <-
+        simpleOptions
+            ("orgstat-" <> showVersion version)
+            "----- OrgStat ------"
+            "Statistic reports visualizer for org-mode"
+            argsParser
+            empty
+    pure res
 
 main :: IO ()
-main = putText "Hurp durp"
+main = do
+    args <- getNodeOptions
+    when (debug args) $ putText $ "Was launched with options: " <> show args
