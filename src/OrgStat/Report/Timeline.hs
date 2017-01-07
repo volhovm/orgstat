c24166936b84cdc26c03b263e33dda6fdde89ec7
Evgeny Vinogradov <zhenyavinogradov@gmail.com>
2017-01-08 02:38:26 +0300

    Add single day timeline

--- src/OrgStat/Report/Timeline.hs
+++ src/OrgStat/Report/Timeline.hs
@@ -5,11 +5,18 @@ module OrgStat.Report.Timeline
        , processTimeline
        ) where
 
+import qualified Data.Attoparsec.Text as A
 import           Data.Default         (Default (..))
+import qualified Data.Text            as T
+import           Data.Time            (Day, DiffTime, UTCTime (..), fromGregorian)
+import           Diagrams.Backend.SVG (B)
+import qualified Diagrams.Backend.SVG as DB
+import qualified Diagrams.Prelude     as D
+import           OrgStat.Parser       (parseOrg)
 import           Universum
 
-import           OrgStat.Ast          (Org)
-import           OrgStat.Report.Types (SVGImageReport)
+import           OrgStat.Ast          (Clock (..), Org (..))
+import           OrgStat.Report.Types (SVGImageReport (..))
 
 data TimelineParams = TimelineParams
     { _tpColorSalt   :: Int    -- ^ Salt added when getting color out of task name.
@@ -20,6 +27,47 @@ data TimelineParams = TimelineParams
 
 instance Default TimelineParams where
     def = TimelineParams 0 True 5 1
+
+diffTimeToMinutes :: DiffTime -> Double
+diffTimeToMinutes time = fromInteger $ floor $ (/ 60) $ toRational time
+
+-- timeline for a single day
+timelineDay :: [(Text, (DiffTime, DiffTime))] -> D.Diagram B
+timelineDay clocks = mconcat
+    [ mconcat (map showClock clocks)
+    , background
+    ]
+  where
+    width = 250
+    totalHeight = 24*60
+
+    colour :: Text -> D.Colour Double
+    colour _ = D.pink
+
+    background :: D.Diagram B
+    background =
+      D.rect width totalHeight
+      & D.lw D.none
+      & D.fc D.red
+      & D.moveOriginTo (D.p2 (-width/2, totalHeight/2))
+      & D.moveTo (D.p2 (0, totalHeight))
+
+    showClock :: (Text, (DiffTime, DiffTime)) -> D.Diagram B
+    showClock (label, (start, end)) =
+      let
+        w = width
+        h = diffTimeToMinutes $ end - start
+      in
+        mconcat
+          [ D.alignedText 0 0.5 (T.unpack label)
+            & D.font "DejaVu Sans" & D.fontSize 10
+            & D.moveTo (D.p2 (-w/2+10, 0))
+          , D.rect w h
+            & D.lw (D.output 0.5)
+            & D.fc (colour label)
+          ]
+        & D.moveOriginTo (D.p2 (-w/2, h/2))
+        & D.moveTo (D.p2 (0, totalHeight - diffTimeToMinutes start))
 
 processTimeline :: (MonadThrow m) => TimelineParams -> Org -> m SVGImageReport
 processTimeline = notImplemented
