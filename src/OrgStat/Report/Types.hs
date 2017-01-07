6abd501bd6656e67e80d82a79625afee7a846025
Evgeny Vinogradov <zhenyavinogradov@gmail.com>
2017-01-08 02:31:11 +0300

    Add SVG report type

--- src/OrgStat/Report/Types.hs
+++ src/OrgStat/Report/Types.hs
@@ -4,9 +4,11 @@ module OrgStat.Report.Types
        ( SVGImageReport (..)
        ) where
 
+import           Diagrams.Backend.SVG (B)
+import qualified Diagrams.Prelude     as D
 import           Universum
 
 -- Also thing to think about is how we output settings (time ranges
 -- etc.) -- on the plot, in the corner, in the file name, as a
 -- description file ?
-data SVGImageReport = SVGImage Void -- wrap real SVG image here.
+data SVGImageReport = SVGImage (Double, Double) (D.Diagram B)
