1e99665037fad351701b151de566fe60bbcb3d38
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-01-08 14:12:16 +0300

    Add zlib, fix compilation

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> { };
 haskell.lib.buildStackProject {
    ghc = haskell.packages.ghc801.ghc;
    name = "orgstat";
-   buildInputs = [ git openssh gnupg gnupg1compat ];
+   buildInputs = [ zlib git openssh gnupg gnupg1compat ];
    LANG = "en_US.UTF-8";
 }
 
