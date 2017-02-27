6716db46a7b88c350119abb0171a203c50c75e94
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-02-27 09:20:38 +0300

    Switch to lts 8.2

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> { };
 haskell.lib.buildStackProject {
-   ghc = haskell.packages.ghc801.ghc;
+   ghc = haskell.packages.ghc802.ghc;
    name = "orgstat";
    buildInputs = [ zlib git openssh gnupg gnupg1compat xdg_utils firefox ];
    LANG = "en_US.UTF-8";
