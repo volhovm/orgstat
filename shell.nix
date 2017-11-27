cb4388d358df16856c3b9f468341e79e957a1fff
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-11-27 09:25:23 +0300

    Bump version of log-warper and universum

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> { };
 haskell.lib.buildStackProject {
-   ghc = haskell.packages.ghc802.ghc;
+   ghc = haskell.packages.ghc822.ghc;
    name = "orgstat";
    buildInputs = [ zlib git openssh gnupg gnupg1compat xdg_utils firefox ];
    LANG = "en_US.UTF-8";
