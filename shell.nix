9024e10f9592b9196d49de71fec8f064f728aef7
Mikhail Volkhov <volhovm.cs@gmail.com>
2022-07-17 21:32:03 +0100

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,8 @@
-with import <nixpkgs> { };
+with import (builtins.fetchGit {
+    url = https://github.com/NixOS/nixpkgs;
+    ref = "nixos-20.09";
+    rev = "cd63096d6d887d689543a0b97743d28995bc9bc3";
+}) {};
 haskell.lib.buildStackProject {
    #ghc = haskell.packages.ghc8102.ghc;
    ghc = haskell.packages.ghc882.ghc;
@@ -7,4 +11,3 @@ haskell.lib.buildStackProject {
    buildInputs = [ zlib git openssh gnupg gnupg1compat xdg_utils firefox gmp ];
    LANG = "en_US.UTF-8";
 }
-
